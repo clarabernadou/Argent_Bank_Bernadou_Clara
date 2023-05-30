527334f3426d0429337bf30ff1a47eb75d356bd8
Clara Bernadou <clarabernadou56690@gmail.com>
2023-05-30 02:31:34 +0200

    Change first and last name on profile page

--- frontend/src/components/ProfilePage/Header.jsx
+++ frontend/src/components/ProfilePage/Header.jsx
@@ -1,30 +1,87 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import { useSelector, useDispatch } from 'react-redux';
 import { setUser } from '../../reducers/userReducer';
 
 export default function Header() {
-    const { firstName, lastName } = useSelector(state => state.user);
-    const dispatch = useDispatch();
-  
-    useEffect(() => {
-      const fetchUserData = async () => {
-        const config = {
-          headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token') || sessionStorage.getItem('token')}`
-          }
-        };
-        const response = await axios.post('http://localhost:3001/api/v1/user/profile', {}, config);
-        const userData = response.data.body;
-        dispatch(setUser(userData));
+  const { firstName, lastName } = useSelector(state => state.user);
+  const dispatch = useDispatch();
+
+  const [editing, setEditing] = useState(false);
+  const [editedFirstName, setEditedFirstName] = useState(firstName);
+  const [editedLastName, setEditedLastName] = useState(lastName);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const token = localStorage.getItem('token') || sessionStorage.getItem('token');
+      const config = {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
       };
-      fetchUserData();
-    }, [dispatch]);
+      const response = await axios.post('http://localhost:3001/api/v1/user/profile', {}, config);
+      const userData = response.data.body;
+      dispatch(setUser(userData));
+    };
+
+    fetchUserData();
+  }, [dispatch]);
+
+  const handleEditClick = () => {
+    setEditedFirstName(firstName);
+    setEditedLastName(lastName);
+    setEditing(true);
+  };
+
+  const handleSaveClick = async () => {
+    const token = localStorage.getItem('token') || sessionStorage.getItem('token');
+    const config = {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    };
+
+    try {
+      await axios.put('http://localhost:3001/api/v1/user/profile', { firstName: editedFirstName, lastName: editedLastName }, config);
+      dispatch(setUser({ firstName: editedFirstName, lastName: editedLastName }));
+      setEditing(false);
+    } catch (error) {
+      console.log('Error while saving user data:', error);
+    }
+  };
+
+  const handleCancelClick = () => {
+    setEditing(false);
+  };
 
   return (
     <div className="header">
-        <h1>Welcome back<br />{firstName} {lastName}!</h1>
-        <button className="edit-button">Edit Name</button>
+      {editing ? (
+        <div className="edit-form">
+          <h1>Welcome back</h1>
+          <div className="input-container">
+            <input
+              type="text"
+              placeholder={editedFirstName}
+              onChange={(e) => setEditedFirstName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder={editedLastName}
+              onChange={(e) => setEditedLastName(e.target.value)}
+            />
+          </div>
+          <div className="button-container">
+            <button className="edit-button form-button" onClick={handleSaveClick}>Save</button>
+            <button className="edit-button form-button" onClick={handleCancelClick}>Cancel</button>
+          </div>
+        </div>
+      ) : (
+        <div className="user-info">
+          <h1>Welcome back<br />{firstName} {lastName}!</h1>
+          <button className="edit-button" onClick={handleEditClick}>Edit Name</button>
+        </div>
+      )}
     </div>
   );
 }
