3162209c17461c666eaea278ee94d10c79033d81
Clara Bernadou <clarabernadou56690@gmail.com>
2023-06-07 02:27:02 +0200

    Change localStorage/sessionStorage to cookies

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,10 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
+import Cookies from 'js-cookie';
 
 const userSlice = createSlice({
   name: 'user',
   initialState: {
     firstName: '',
     lastName: '',
+    token: Cookies.get('token'),
   },
   reducers: {
     setUser: (state, action) => {
@@ -12,9 +14,16 @@ const userSlice = createSlice({
       state.firstName = firstName;
       state.lastName = lastName;
     },
+    setToken: (state, action) => {
+      state.token = action.payload;
+    },
+    deleteToken: (state, action) => {
+      state.token = '';
+      Cookies.remove('token');
+    }    
   },
 });
 
-export const { setUser } = userSlice.actions;
+export const { setUser, setToken, deleteToken } = userSlice.actions;
 
 export default userSlice.reducer;
